2fc4c4251bcb0656de28909419f97aeaef92f581
dongwook kim <kyaol4@gmail.com>
2022-02-16 10:50:48 +0900

    commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,8 +13,11 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    @task.save
-    redirect_to task_path(@task)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -23,8 +26,17 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    @task.update(task_params)
-    redirect_to task_path(@task)
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
   get '/tasks/new', to: 'tasks#new', as: :new_task
   post '/tasks', to: 'tasks#create', as: :tasks
 
-  #Read
+  # Read
   get '/tasks', to: 'tasks#index'
   get '/tasks/:id', to: 'tasks#show', as: :task
 
-  #Update
+  # Update
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
+
+  # Delete
+  delete 'tasks/:id', to: 'tasks#delete'
 end
